ce97e60edf33a4028951ce9ac947b5a1f3747453
astrolul <85197489+astrolul@users.noreply.github.com>
2025-11-12 20:15:51 +0000

    adding conditional compilation

--- config.def.h
+++ config.def.h
@@ -1,12 +1,18 @@
 /* See LICENSE file for copyright and license details. */
 /* Default settings; can be overriden by command line. */
+#ifndef FONT_SIZE
+#define FONT_SIZE 10
+#endif
+#ifndef BAR_HEIGHT
+#define BAR_HEIGHT 8
+#endif
 
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
 /* -fn option overrides fonts[0]; default X11 font or font set */
-static const int user_bh = 9;               /* add an defined amount of pixels to the bar height */
+static const int user_bh = BAR_HEIGHT;               /* add an defined amount of pixels to the bar height */
 
 static const char *fonts[] = {
-	"Fira Code:style=Regular:size=14"
+	"Fira Code:style=Regular:size=" TOSTRING(FONT_SIZE)
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
